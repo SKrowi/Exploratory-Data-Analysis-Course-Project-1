ac614acb405554a703c3732a5383019744eda25c
Stefanie Krowinnus - SCP <sk@scpartners.at>
2020-12-27 17:34:44 +0100

    Upload Plot 2 + Code

---
+++ plot2.R
@@ -0,0 +1,29 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 01. Loading the Data ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+df_raw <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses =
+                       c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+library(tidyverse) # e.g. for piping 
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 02. Cleaning the Data ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+df_clean <- df_raw %>%
+  mutate(Date = as.Date(Date, "%d/%m/%Y")) %>% # Date to Type Date
+  filter(Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2")) %>% # Date Filter
+  drop_na() %>% # Drop Incomplete Observations
+  unite(DateTime, Date, Time, sep = " ", remove = TRUE) %>% # Join Date + Time 
+  mutate(DateTime = as.POSIXct(DateTime)) # Format to Date Time 
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 03. Plot Design ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+plot(df_clean$Global_active_power ~ df_clean$DateTime, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 04. Plot Export ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+dev.cur()
+dev.copy(png,"plot2.png", width = 480, height = 480)
+dev.off()
